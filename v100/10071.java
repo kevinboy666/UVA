ecf8a826ae3cb80045c07750f41d64d8bedaa54b
kevinboy666 <70878493+kevinboy666@users.noreply.github.com>
2021-03-20 15:09:55 +0800

    Create 10071.java

---
+++ v100/10071.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int v=0,t=0,dp=0;
+        while (sc.hasNext()) {
+            v=sc.nextInt();
+            t=sc.nextInt();
+            System.out.println(v*(t*2));
+            }
+        }
+    }
