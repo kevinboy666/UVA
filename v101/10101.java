397c0417373958a255a4714c01e8341cba701d11
kevinboy666 <70878493+kevinboy666@users.noreply.github.com>
2021-10-13 14:49:50 +0800

    Update 10101.java

--- v101/10101.java
+++ v101/10101.java
@@ -1,45 +1,45 @@
-
 import java.util.*;
-public class Main{
-	public static void main(String agrv[]) {
-		Scanner sc = new Scanner(System.in);
-		int count=0;
-		while (sc.hasNextLong()) {
-			count++;
-			System.out.printf("%4d.", count);
-			long num = sc.nextLong();
-			if(num==0) {
-				System.out.print(" "+num+"\n");
-				continue;
-			}
-			Bangla(num);
-			System.out.print("\n");
-		}
-	}
-	public static void Bangla(long num) {		
-		if (num >= 10000000) {
-			Bangla(num/10000000);
-			System.out.print(" kuti");
-			num = num % 10000000;
-		}
-		if (num >= 100000) {
-			Bangla(num/100000);
-			System.out.print(" lakh");
-			num = num % 100000;
-		}
-		if (num >= 1000) {
-			Bangla(num/1000);
-			System.out.print(" hajar");
-			num = num % 1000;
-		}
-		if (num >= 100) {
-			Bangla(num/100);
-			System.out.print(" shata");
-			num=num%100;
-			if(num!=0)
-			System.out.print(" "+num);
-		}	
-		else if(num!=0)
-			System.out.print(" "+num);
-	}
+
+public class Main {
+    public static void main(String[] agrv) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while (sc.hasNextLong()) {
+            count++;
+            System.out.printf("%4d.", count);
+            long num = sc.nextLong();
+            if (num == 0) {
+                System.out.println(" " + num);
+                continue;
+            }
+            Bangla(num);
+            System.out.println();
+        }
+    }
+
+    public static void Bangla(long num) {
+        if (num >= 10000000) {
+            Bangla(num / 10000000);
+            System.out.print(" kuti");
+            num = num % 10000000;
+        }
+        if (num >= 100000) {
+            Bangla(num / 100000);
+            System.out.print(" lakh");
+            num = num % 100000;
+        }
+        if (num >= 1000) {
+            Bangla(num / 1000);
+            System.out.print(" hajar");
+            num = num % 1000;
+        }
+        if (num >= 100) {
+            Bangla(num / 100);
+            System.out.print(" shata");
+            num = num % 100;
+            if (num != 0)
+                System.out.print(" " + num);
+        } else if (num != 0)
+            System.out.print(" " + num);
+    }
 }
