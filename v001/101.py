fb3b21e19ad80146874698989ee935a664a1bd77
kevinboy666 <kevinboy665@gmail.com>
2022-06-19 23:18:35 +0800

    print matrix

--- v001/101.py
+++ v001/101.py
@@ -1,10 +1,16 @@
 from sys import stdin, stdout
+
+
 n=int(input())
-commands=stdin.readlines()
-# input=readline()
-# n=int(input[0])
-# commands=input[1:]
-# blocks=[[0 for y in range(n)] for x in range(n)]
+commands=stdin.read().splitlines()
+blocks=[[0 for y in range(n)] for x in range(n)]
 
 for i in commands:
-    print(i, end='')
+    command=i.split(' ')
+    print(command)
+
+for r in range(len(blocks)):
+    print(f'{r}:',end='')
+    for c in range(len(blocks[r])):
+        print(f' {blocks[r][c]}',end='')
+    print('')
